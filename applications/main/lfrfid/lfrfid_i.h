0c23384e729a4084dfdb25cd77c88b7fb83f082b
m5performance <65921900+m5performance@users.noreply.github.com>
2022-12-01 01:06:36 +0300

    Move Infrared Back to Internal

--- applications/main/lfrfid/lfrfid_i.h
+++ applications/main/lfrfid/lfrfid_i.h
@@ -7,6 +7,7 @@
 #include <gui/view.h>
 #include <gui/view_dispatcher.h>
 #include <gui/scene_manager.h>
+#include <cli/cli.h>
 #include <notification/notification_messages.h>
 
 #include <gui/modules/submenu.h>
